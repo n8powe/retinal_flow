adc3abee11dc8bef7fb085cfc251b022c9379010
Baptiste Caziot <baptiste.caziot@gmail.com>
2022-11-09 18:59:14 +0100

    Added target detection

--- baptiste/retinal_flow_2_processScene.py
+++ baptiste/retinal_flow_2_processScene.py
@@ -15,10 +15,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # Scene video name and path
 dataPath = '../../retinal_flow_data/'
-fileName = 'eye_2022-11-08_19-54-09'
+fileName = 'scene_2022-11-08_19-54-09'
 
 # Open video and trigger files
 vidIn = cv2.VideoCapture('%s/%s_processed.avi' % (dataPath, fileName))
@@ -30,6 +29,7 @@ imageSize = (int(vidIn.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vidIn.get(cv2.CAP_PRO
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 writer = cv2.VideoWriter('%s/%s_trimmed.mp4' % (dataPath, fileName), fourcc, 30, imageSize)
 
+# Detect when tasks started and ended (from the trigger)
 trigOn, = np.where(data[1:, 1] - data[:-1, 1] > 0.5)
 trigOff, = np.where(data[1:, 1] - data[:-1, 1] < -0.5)
 trigDur = trigOff - trigOn
@@ -39,100 +39,96 @@ taskStop = trigOff[trigDur > 80]
 # if not os.path.exists('%s/%s_charuco' % (dataPath, fileName)):
 #     os.mkdir('%s/%s_charuco' % (dataPath, fileName))
 
-gridSize = (14, 7)
-boxWidthPix = 8 * 16
-cornerThreshold = 4
-dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-board = cv2.aruco.CharucoBoard_create(gridSize[0], gridSize[1], boxWidthPix, 0.75 * boxWidthPix, dictionary)
 
-
-print("Detect charuco board")
-allCharucoCorners = []
-allCharucoIds = []
-vidIn.set(cv2.CAP_PROP_POS_FRAMES, taskStart[0]+1)
-for ff in range(taskStart[0]+1,taskStop[0]):
-    ret, frame = vidIn.read()
-    # cv2.imwrite('%s/%s_charuco/%s_charuco_%d.png' % (dataPath,fileName,fileName,ff) , frame)
-    writer.write(frame)
-
-    arucoCorners, arucoIds, arucoRejected = cv2.aruco.detectMarkers(frame,dictionary)
-    cv2.aruco.drawDetectedMarkers(frame,arucoCorners, arucoIds, borderColor=(0,0,255));
-
-    if len(arucoCorners)>0:
-        charucoNumber, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(arucoCorners, arucoIds, frame, board);
-
-        if charucoNumber>cornerThreshold:
-            allCharucoCorners.append(charucoCorners)
-            allCharucoIds.append(charucoIds)
-
-    cv2.imshow('picture', frame)
-    cv2.waitKey(1)
-
-writer.release()
-cv2.waitKey(-1)
-
-
-
+# # Generate the same charuco board used for calibration
+# gridSize = (14, 7)
+# boxWidthPix = 8 * 16
+# cornerThreshold = 4
+# dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+# board = cv2.aruco.CharucoBoard_create(gridSize[0], gridSize[1], boxWidthPix, 0.75 * boxWidthPix, dictionary)
 #
-# # retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(allCharucoCorners, allCharucoIds, board, imageSize, None, None)
+# # Extract frames from the 1st trigger where the charuco was displayed and detect it
+# print("Detect charuco board")
+# allCharucoCorners = []
+# allCharucoIds = []
+# vidIn.set(cv2.CAP_PROP_POS_FRAMES, taskStart[0] + 1)
+# for ff in range(taskStart[0] + 1, taskStop[0]):
+#     ret, frame = vidIn.read()
+#     arucoCorners, arucoIds, arucoRejected = cv2.aruco.detectMarkers(frame, dictionary)
+#     cv2.aruco.drawDetectedMarkers(frame, arucoCorners, arucoIds, borderColor=(0, 0, 255))
+#     writer.write(frame)
 #
-# # newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 1, imageSize)
+#     if len(arucoCorners) > 0:
+#         charucoNumber, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(arucoCorners, arucoIds, frame,
+#                                                                                         board)
 #
+#         if charucoNumber > cornerThreshold:
+#             allCharucoCorners.append(charucoCorners)
+#             allCharucoIds.append(charucoIds)
+#
+#     cv2.imshow('picture', frame)
+#     cv2.waitKey(1)
+#
+# writer.release()
+# cv2.destroyAllWindows()
+#
+# # Perform camera calibration from charuco board
+# print("Perform camera calibration")
+# retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(allCharucoCorners, allCharucoIds,
+#                                                                                   board, imageSize, None, None)
+# newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 1, imageSize)
 #
 # # vidIn.set(cv2.CAP_PROP_POS_FRAMES, taskStart[0]+1)
 # # for ff in range(taskStart[0]+1,taskStop[0]):
 # #     ret, frame = vidIn.read()
 # #     # cv2.imwrite('%s/%s_charuco/%s_charuco_%d.png' % (dataPath,fileName,fileName,ff) , frame)
-#
 # #     undistorted = cv2.undistort(frame, cameraMatrix, distCoeffs, None, newcameramtx)
-#
 # #     # cv2.imshow('picture', undistorted)
 # #     # cv2.waitKey(1)
-#
-#
-# print("Detect targets")
-# # targetPosition = pd.DataFrame(columns = ['T','X','Y'])
-# data2 = pd.DataFrame(data=data)
-# data2 = data2.assign(tgX=np.full(data2.shape[0], np.nan))
-# data2 = data2.assign(tgY=np.full(data2.shape[0], np.nan))
-#
-# marginX = 0  # int(np.min(allCharucoCorners[0][:,0,0]))
-# marginY = 0  # int(np.min(allCharucoCorners[0][:,0,1]))
-#
-# vidIn.set(cv2.CAP_PROP_POS_FRAMES, taskStart[1] + 1)
-# for ff in range(taskStart[1] + 1, taskStop[1]):
-#     ret, frame = vidIn.read()
-#     # cv2.imwrite('%s/%s_charuco/%s_charuco_%d.png' % (dataPath,fileName,fileName,ff) , frame)
-#
-#     gray = frame[marginX:frame.shape[0], marginY:frame.shape[1]]
-#     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-#
-#     # undistorted = cv2.undistort(gray, cameraMatrix, distCoeffs, None, newcameramtx)
-#     gray = cv2.medianBlur(gray, 5)
-#
-#     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 1,
-#                                param1=50, param2=10,
-#                                minRadius=5, maxRadius=20)
-#
-#     if circles is not None:
-#         tgX = int(np.mean(circles[:, :, 0])) + marginX
-#         tgY = int(np.mean(circles[:, :, 1])) + marginY
-#         cv2.circle(gray, (tgX, tgY), 10, (255, 255, 255), 3)
-#
-#         data2['tgX'].loc[ff] = tgX
-#         data2['tgY'].loc[ff] = tgY
-#         # data2.assign(tgX=np.full(data2.shape[0],np.nan))
-#         # targetPosition = targetPosition.append({'T':ff,'X': tgX,'Y': tgY},ignore_index=True)
-#         # loc[len(eyePosition.index)] = [result_2d["ellipse"]['center'][0],result_2
-#
-#     cv2.imshow("detected circles", gray)
-#     cv2.waitKey(-1)
-#
-# # save the dataframe as a csv file
-# # targetPosition.to_csv('%s/%s_targetPosition.csv' % (dataPath,fileName))
-# data2.to_csv('%s/%s_targetPosition.csv' % (dataPath, fileName))
+
+
+# Now extract frames where the eye calibration target was displayed. The eye calibration target is a black bullseye
+# surrounded by 4 arucos
+print("Detect targets")
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+# markersNumber = 4
+# for mm in range(0, markersNumber):
+#     img = cv2.aruco.drawMarker(dictionary, mm, 6)
+# # board = cv2.aruco.CharucoBoard_create(gridSize[0], gridSize[1], boxWidthPix, 0.75 * boxWidthPix, dictionary)
+
+targetPosition = pd.DataFrame(data=data)
+targetPosition = targetPosition.assign(tgX=np.full(targetPosition.shape[0], np.nan))
+targetPosition = targetPosition.assign(tgY=np.full(targetPosition.shape[0], np.nan))
+
+marginX = 0  # int(np.min(allCharucoCorners[0][:,0,0]))
+marginY = 0  # int(np.min(allCharucoCorners[0][:,0,1]))
+
+# Move video to start of task 2 (calibration)
+vidIn.set(cv2.CAP_PROP_POS_FRAMES, taskStart[1] + 1)
+for ff in range(taskStart[1] + 1, taskStop[1]):
+    ret, frame = vidIn.read()
+
+    arucoCorners, arucoIds, arucoRejected = cv2.aruco.detectMarkers(frame, dictionary)
+    cv2.aruco.drawDetectedMarkers(frame, arucoCorners, arucoIds, borderColor=(0, 0, 255))
+
+    if len(arucoCorners) == 4:
+        targetX = np.empty(4)
+        targetY = np.empty(4)
+        for aa in range(0, 4):
+            targetX[aa] = 0.25 * (arucoCorners[aa][0][0][0] + arucoCorners[aa][0][1][0] +
+                                  arucoCorners[aa][0][2][0] + arucoCorners[aa][0][3][0])
+            targetY[aa] = 0.25 * (arucoCorners[aa][0][0][1] + arucoCorners[aa][0][1][1] +
+                                  arucoCorners[aa][0][2][1] + arucoCorners[aa][0][3][1])
+        targetPosition['tgX'].loc[ff] = np.mean(targetX)
+        targetPosition['tgY'].loc[ff] = np.mean(targetY)
+        #         # data2.assign(tgX=np.full(data2.shape[0],np.nan))
+        cv2.circle(frame, (int(np.mean(targetX)), int(np.mean(targetY))), 10, (0, 0, 255), -1)
+
+    cv2.imshow('picture', frame)
+    cv2.waitKey(10)
+
+# save the dataframe as a csv file
+targetPosition.to_csv('%s/%s_targetPosition.csv' % (dataPath, fileName))
 
 vidIn.release()
-
-cv2.waitKey(-1)
 cv2.destroyAllWindows()
